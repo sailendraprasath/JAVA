88691421db8248888c9000503ee130d87ceef0c5
sailendraprasath <sailendraprasath05@gmail.com>
2025-01-20 23:58:51 +0530

    worst

---
+++ jan20/work.java
@@ -0,0 +1,11 @@
+class work{
+    public static void main(String[] args) {
+        int mark = 50;
+        if(mark > 35){
+            System.out.println("pass");
+        }
+        else{
+            System.out.println("Fail");
+        }
+    }
+}
